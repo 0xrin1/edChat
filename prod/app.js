eb49b3ab25bcabf9420c7eb9e2f48ea65d666e21
Martin Stuessy <mstuessyosu@gmail.com>
2018-10-06 06:32:29 +0100

    remove prod files

--- prod/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv = require("dotenv");
-dotenv.config();
-const edChat_1 = require("./edChat");
-new edChat_1.default(parseInt(process.env.PORT), process.env.TELEGRAM_TOKEN, () => {
-    console.log('EdChat started');
-});
-//# sourceMappingURL=app.js.map
--- prod/utilities/toolbox.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Toolbox {
-    deleteFromArray(arr, arrIndex) {
-        return arr.slice(0, arrIndex).concat(arr.slice(arrIndex + 1));
-    }
-}
-exports.default = Toolbox;
-//# sourceMappingURL=toolbox.js.map
